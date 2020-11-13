a45398841962c823633b5af2f450417b842a0645
Wes Bos <wesbos@gmail.com>
2020-11-13 16:45:00 -0500

    transcripts Work?

--- components/Transcript.js
+++ components/Transcript.js
@@ -11,8 +11,23 @@ export default function Transcript({ transcript }) {
 }
 
 function TranscriptLine({ line }) {
+  if (!line.text) return '';
+  console.log(line);
   return (
-    <div>
+    <div className={`line line--${line.initials}`}>
+      <header>
+        {line.speaker === 'Wes Bos' && (
+          <img src="https://avatars.githubusercontent.com/wesbos?size=200" />
+        )}
+        {line.speaker === 'Scott Tolinski' && (
+          <img src="https://avatars.githubusercontent.com/stolinski?size=200" />
+        )}
+        <strong>{line.speaker}</strong>
+        <span className="time">
+          {line.timestamp.hh !== '00' ? `${line.timestamp.hh[1]}:` : ''}
+          {line.timestamp.mm}:{line.timestamp.ss}
+        </span>
+      </header>
       <p>{line.text}</p>
     </div>
   );
